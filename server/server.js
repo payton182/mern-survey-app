2aa86c398da5d8aacd8ba90ccfe943a7f8c50e0b
Payton Clark <payton182@gmail.com>
2019-01-16 01:41:24 -0600

    updates

---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+require('./routes/authRoutes')(app);
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.listen(port, () => {
+  console.log(`Server is up on ${port}`);
+});
